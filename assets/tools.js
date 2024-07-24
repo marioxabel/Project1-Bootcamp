b21ad64c5fa8cd4685735ffdc66e6f5ae5000983
Mario <31260739+marioxabel@users.noreply.github.com>
2024-07-23 21:00:16 -0600

    Final version

--- assets/tools.js
+++ assets/tools.js
@@ -1,17 +1,17 @@
 function generateRandomID() {
-    return Math.floor(Math.random()*10000000)
+  return Math.floor(Math.random() * 10000000)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const dateOptions = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    };
-    const currentDate = new Date().toLocaleDateString('en-US', dateOptions);
-    document.getElementById("current-date").textContent = currentDate;
-  });
+  const dateOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  };
+  const currentDate = new Date().toLocaleDateString('en-US', dateOptions)
+  document.getElementById("current-date").textContent = currentDate
+})
 
